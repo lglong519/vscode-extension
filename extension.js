7a1ba7fe77e6977a320ab16f7e74b23226802b58
lglong519 <769141564@qq.com>
2018-04-26 00:38:44 +0800

    extension.run done

--- extension.js
+++ extension.js
@@ -1,26 +1,48 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const statusBarItems = [];
 
+function addStatusBarItem(str, cmd, tip, col) {
+    statusBarItems.push(vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left));
+    statusBarItems[statusBarItems.length - 1].text = str;
+    if (cmd) statusBarItems[statusBarItems.length - 1].command = cmd;
+    if (tip) statusBarItems[statusBarItems.length - 1].tooltip = tip;
+    if (col) statusBarItems[statusBarItems.length - 1].color = col;
+    statusBarItems[statusBarItems.length - 1].show();
+}
+
+let terminal = vscode.window.createTerminal({ name: "lglong519" });
+terminal.show(true);
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
+    
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "9527" is now active!');
-
+    addStatusBarItem('|');
+    addStatusBarItem('Run', 'extension.run', 'Run current file', 'red');
+    addStatusBarItem('|');
+    addStatusBarItem('Clear', 'extension.clear', 'clearTerminal', 'yellow');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let runFileInTerminal = vscode.commands.registerCommand('extension.run', function () {
+        let filePath = String(vscode.window.activeTextEditor.document.uri).split('//').pop()
+        //execute cmd
+        if (filePath.endsWith('.js')) {
+            terminal.sendText(`node ${filePath}`);
+        } else {
+            vscode.window.setStatusBarMessage('Not a JS file.',3000);
+        }
     });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(runFileInTerminal);
+    let clearTerminal = vscode.commands.registerCommand('extension.clear', function () {
+        terminal.dispose();
+        terminal = vscode.window.createTerminal({ name: "lglong519" });
+        terminal.show(true);
+    });
+    context.subscriptions.push(clearTerminal);
 }
 exports.activate = activate;
 
