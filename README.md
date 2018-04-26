c554a0cbee475f61e95e6690d384932b95457111
lglong519 <769141564@qq.com>
2018-04-26 12:16:25 +0800

    add a rerun button,supplement readme  with extension details

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 #About
-      run the file which is openning and active.
-#Install
-      vsix run-0.0.1.vsix
+      run the js file which is openning and active.
+##Install vsce
+      npm install -g vsce
+##Packaging extension
+      vsce package
+##Install extension
+      code --install-extension run-0.0.1.vsix
--- extension.js
+++ extension.js
@@ -3,45 +3,31 @@
 const vscode = require('vscode');
 const statusBarItems = [];
 
-function addStatusBarItem(str, cmd, tip, col) {
-    statusBarItems.push(vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left));
-    statusBarItems[statusBarItems.length - 1].text = str;
-    if (cmd) statusBarItems[statusBarItems.length - 1].command = cmd;
-    if (tip) statusBarItems[statusBarItems.length - 1].tooltip = tip;
-    if (col) statusBarItems[statusBarItems.length - 1].color = col;
-    statusBarItems[statusBarItems.length - 1].show();
-}
-
 let terminal = vscode.window.createTerminal({ name: "lglong519" });
 terminal.show(true);
+let outputChannel = vscode.window.createOutputChannel("lglong519");
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
     
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     addStatusBarItem('|');
     addStatusBarItem('Run', 'extension.run', 'Run current file', 'red');
+    addStatusBarItem('|');
+    addStatusBarItem('Rerun', 'extension.rerun', 'Run current file again');
     addStatusBarItem('|');
     addStatusBarItem('Clear', 'extension.clear', 'clearTerminal', 'yellow');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let runFileInTerminal = vscode.commands.registerCommand('extension.run', function () {
-        let filePath = String(vscode.window.activeTextEditor.document.uri).split('//').pop()
-        //execute cmd
-        if (filePath.endsWith('.js')) {
-            terminal.sendText(`node ${filePath}`);
-        } else {
-            vscode.window.setStatusBarMessage('Not a JS file.',3000);
-        }
-    });
+    let runFileInTerminal = vscode.commands.registerCommand('extension.run',runFile);
     context.subscriptions.push(runFileInTerminal);
-    let clearTerminal = vscode.commands.registerCommand('extension.clear', function () {
-        terminal.dispose();
-        terminal = vscode.window.createTerminal({ name: "lglong519" });
-        terminal.show(true);
+    let reRunFileInTerminal = vscode.commands.registerCommand('extension.rerun', function () {
+        reStartTerminal();
+        runFile();
     });
+    context.subscriptions.push(reRunFileInTerminal);
+    let clearTerminal = vscode.commands.registerCommand('extension.clear',reStartTerminal);
     context.subscriptions.push(clearTerminal);
 }
 exports.activate = activate;
@@ -50,3 +36,27 @@ exports.activate = activate;
 function deactivate() {
 }
 exports.deactivate = deactivate;
+
+function addStatusBarItem(str, cmd, tip, col) {
+    statusBarItems.push(vscode.window.createStatusBarItem(vscode.StatusBarAlignment.right));
+    statusBarItems[statusBarItems.length - 1].text = str;
+    if (cmd) statusBarItems[statusBarItems.length - 1].command = cmd;
+    if (tip) statusBarItems[statusBarItems.length - 1].tooltip = tip;
+    if (col) statusBarItems[statusBarItems.length - 1].color = col;
+    statusBarItems[statusBarItems.length - 1].show();
+}
+function runFile(){
+    let filePath = String(vscode.window.activeTextEditor.document.uri).split('//').pop()
+        //execute cmd
+        if (filePath.endsWith('.js')) {
+            terminal.sendText(`node ${filePath}`);
+        } else {
+            vscode.window.setStatusBarMessage('Not a JS file.',3000);
+            outputChannel.append('Not a JS file.');
+        }
+}
+function reStartTerminal(){
+    terminal.dispose();
+    terminal = vscode.window.createTerminal({ name: "lglong519" });
+    terminal.show(true);
+}
